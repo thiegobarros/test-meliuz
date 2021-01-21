52e4c363ae6f61fa36fe1ba89c1cf41a6f6eea09
Thiego Barros <thiegobarros@gmail.com>
2021-01-20 20:10:29 -0400

    criando estruturas e rotas

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Details from '../pages/Details';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/details" exact component={Details} />
+    </Switch>
+  );
+}
+
+export default Routes;
